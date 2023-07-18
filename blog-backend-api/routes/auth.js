4cd744db4b6cecdbd923cb4fce254c4455d37a24
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2023-07-18 14:52:16 -0400

    added delete user function

--- blog-backend-api/routes/auth.js
+++ blog-backend-api/routes/auth.js
@@ -59,6 +59,24 @@ router.delete("/logout", (req, res) => {
   });
 });
 
+router.delete('/users/:id', async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const user = await User.findByPk(userId);
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      await user.destroy();
+  
+      return res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
 router.post("/login", async (req, res) => {
     try {
         const { username, password } = req.body;
